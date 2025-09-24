6dac892b350ddd12dbd626bf5492a04d919571b1
saieda <saieda@udel.edu>
2025-09-24 11:29:13 -0400

    adding script for mac

--- frontend/scripts/dev-api-gen.js
+++ frontend/scripts/dev-api-gen.js
@@ -1,10 +1,11 @@
 const execSync = require('child_process').execSync;
 const fs = require('fs');
 
-const output = execSync('ipconfig | findstr -i \"ipv4\"').toString();
+//const output = execSync('ipconfig | findstr -i \"ipv4\"').toString();
+const output = execSync('ifconfig | grep "inet "').toString();
 let ip;
 try{
-    ip = output.match(/\d+\.\d+\.\d+\.\d+/)[0];
+    ip = output.slice(20).match(/\d+\.\d+\.\d+\.\d+/)[0];
 }
 catch(e){
     console.warn("Failed to retrieve IP configuration: .apiconfig.json file not generated. Error:", e);
