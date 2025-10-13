41c1e0ea063c817f7d884a8b2f75d3f07539d8bf
Olivia <okarney@udel.edu>
2025-10-13 17:02:21 -0400

    Added basic fields to java class and getter/setter methods.

---
+++ backend/src/main/java/com/ud11/groceries/classes/User.java
@@ -0,0 +1,53 @@
+package com.ud11.groceries.classes;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    // private _______ allergies;
+    // private _______ dietaryRestrictions;
+
+    public User(String firstName, String lastName, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+
+    }
+
+    // Getter Methods
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+    // Setter Methods
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
