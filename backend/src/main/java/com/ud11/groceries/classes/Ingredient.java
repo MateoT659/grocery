3ab5b05c7a4e3547572bb54703272e890a4d10a2
Mateo Torres <mateot659@gmail.com>
2025-10-13 14:55:29 -0400

    edit to allergies and ingredients

--- backend/src/main/java/com/ud11/groceries/classes/Ingredient.java
+++ backend/src/main/java/com/ud11/groceries/classes/Ingredient.java
@@ -1,4 +1,12 @@
 package com.ud11.groceries.classes;
 
+import java.util.ArrayList;
+
 public class Ingredient {
+    private String name;
+    private String description;
+    private String unit; // e.g., grams, liters, pieces, how is this ingredient measured
+    private ArrayList<Allergies> allergens; // list of allergensgit a
+    private ArrayList<Diets> diets; // suitable for which diets
+
 }
