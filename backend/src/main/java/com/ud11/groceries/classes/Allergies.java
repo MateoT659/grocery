2553c1c70b7c5b40546cfc4a92f4bba4de5962e2
Mateo Torres <mateot659@gmail.com>
2025-10-13 12:05:16 -0400

    added allergies and iets

---
+++ backend/src/main/java/com/ud11/groceries/classes/Allergies.java
@@ -0,0 +1,13 @@
+package com.ud11.groceries.classes;
+
+public enum Allergies {
+    GLUTEN,
+    SHELLFISH,
+    EGGS,
+    FISH,
+    PEANUTS,
+    SOYBEANS,
+    TREE_NUTS,
+    SESAME_SEEDS,
+    DAIRY
+}
---
+++ backend/src/main/java/com/ud11/groceries/classes/Diets.java
@@ -0,0 +1,11 @@
+package com.ud11.groceries.classes;
+
+public enum Diets {
+    VEGETARIAN,
+    VEGAN,
+    PESCATARIAN,
+    HALAL,
+    KOSHER,
+    GLUTEN_FREE,
+
+}
