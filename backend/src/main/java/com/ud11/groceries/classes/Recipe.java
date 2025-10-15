f3fc0b070a478a82386a6882b9169253f07f1c97
saieda <saieda@udel.edu>
2025-10-14 23:54:42 -0400

    created the getters and setter for the Recipe class

--- backend/src/main/java/com/ud11/groceries/classes/Recipe.java
+++ backend/src/main/java/com/ud11/groceries/classes/Recipe.java
@@ -1,13 +1,37 @@
 package com.ud11.groceries.classes;
 
-
 public class Recipe {
     //attributes
-    String name;
-    String ingredients;
-    int time;
-    String description;
+    private String name;
+    private String[] ingredients;
+    private int time;
+    private String description;
 
+    //constructor
+    public Recipe(String name, String[] ingredients, int time, String description) {
+        this.name = name;
+        this.ingredients = ingredients;
+        this.time = time;
+        this.description = description;
+    }
+
+
+    //getter
+    public String getName() {
+        return name;
+    }
+    // Setter
+    public void setName(String name) {
+        this.name = name;
+    }
+    //getter
+    public String[] getIngredients() {
+        return ingredients;
+    }
+    // Setter
+    public void setIngredients(String[] ingredients) {
+        this.ingredients = ingredients;
+    }
 
 
     public static void main (String[] args) {
