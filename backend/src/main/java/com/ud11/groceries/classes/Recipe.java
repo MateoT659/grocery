550bda1c17d2408bf9147d0b38e333742c7d74ae
saieda <saieda@udel.edu>
2025-10-14 23:10:02 -0400

    attribures are defined in recipe.

--- backend/src/main/java/com/ud11/groceries/classes/Recipe.java
+++ backend/src/main/java/com/ud11/groceries/classes/Recipe.java
@@ -1,6 +1,18 @@
 package com.ud11.groceries.classes;
 
+
 public class Recipe {
+    //attributes
+    String name;
+    String ingredients;
+    int time;
+    String description;
 
 
+
+    public static void main (String[] args) {
+
+    }
 }
+
+
