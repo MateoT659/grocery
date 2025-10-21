88d26c4c5a1632f4966d8140ba84c57e58df0493
saieda <saieda@udel.edu>
2025-10-20 22:20:19 -0400

    functions for removing recent search and reuse recent search are created.

--- frontend/app/(tabs)/Feed/Search.tsx
+++ frontend/app/(tabs)/Feed/Search.tsx
@@ -4,31 +4,41 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import {Ionicons} from '@expo/vector-icons'
 
 interface SearchModalComp {
-  visible: boolean;
-  onclose: () => void;
-  onsearch: (query: string) => void;  
+  Visible: boolean;
+  OnClose: () => void;
+  OnSearch: (query: string) => void;  
 }
 
-export default function SearchModal ({visible, onclose, onsearch}: SearchModalComp) {
+export default function SearchModal ({Visible, OnClose, OnSearch}: SearchModalComp) {
 
 const [searchQuery, setSearchQuery] = useState('');
 const [recentSearches, setRecentSearches] = useState(['']);
 
 //function to active search button
-const handlesearch = () => {
+const handleSearch = () => {
   if (searchQuery.trim()){
     if (!recentSearches.includes(searchQuery.trim())){
       setRecentSearches([searchQuery.trim(), ...recentSearches]);
     }
   }
-  onsearch(searchQuery);
+  OnSearch(searchQuery);
   setSearchQuery('');
-  onclose();
+  OnClose();
 }
 
-// cancle option 
+//cancle option 
 const handleclose = () => {
   setSearchQuery('');
-  onclose();
+  OnClose();
+}
+
+//removes recent search
+const removesearch = (term : string) => {
+  setRecentSearches(recentSearches.filter(item => item !== term));
+}
+
+//use recent search to search- history
+const searchpress = (term: string) => {
+  setSearchQuery(term);
 }
 }
