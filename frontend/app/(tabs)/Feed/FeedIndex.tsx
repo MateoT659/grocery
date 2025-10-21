efeb1d473452b69817d94507bbb9eaf7f405619c
saieda <saieda@udel.edu>
2025-10-20 23:18:31 -0400

    Feed Page in updated and connected to search page.

--- frontend/app/(tabs)/Feed/FeedIndex.tsx
+++ frontend/app/(tabs)/Feed/FeedIndex.tsx
@@ -5,93 +5,121 @@ import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { Link } from 'expo-router';
-import {useState } from 'react';
-import {Searchbar} from 'react-native-paper'
-import { useRouter } from 'expo-router';
-
+import { useState, useRef } from 'react';
+import { Searchbar } from 'react-native-paper';
+import { TextInput } from 'react-native';
+import SearchModal from './Search';
 
 export default function HomeScreen() {
   // searchbar state
-  const [searchQuery, setSearchQuery] = useState(''); 
-  const router = useRouter();
-  
+  const [searchQuery, setSearchQuery] = useState('');
+  // search page 
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const searchbarRef = useRef<TextInput>(null);
+
+  const handleSearchPress = () => {
+    setIsModalVisible(true);
+    if (searchbarRef.current) {
+      searchbarRef.current.blur();
+    }
+  };
+
+  const handleCloseModal = () => {
+    setIsModalVisible(false);
+    setSearchQuery('');
+  };
+
+  const handleSearch = (query: string) => {
+    console.log('Searching for:', query);
+    // search logic here
+    // Store recent searches in AsyncStorage if needed
+  };
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <>
+      <ParallaxScrollView
+        headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+        headerImage={
+          <Image
+            source={require('@/assets/images/partial-react-logo.png')}
+            style={styles.reactLogo}
+          />
+        }>
+        <Searchbar
+          ref={searchbarRef} placeholder="Search" value={searchQuery} onChangeText={setSearchQuery} onFocus={handleSearchPress} onSubmitEditing={() => {
+            //search in the database for things, store recent searches in local storage
+          }}
+          traileringIcon={'filter'}
+          onTraileringIconPress={() => {
+            //open filter modal
+            alert('Filter button pressed');
+          }}
         />
-      }>
-      <Searchbar placeholder="Search" value={searchQuery} onChangeText={setSearchQuery} onPressIn={() => router.push('/(tabs)/Feed/Search')} onSubmitEditing={() => {
-        //search in the database for things, store recent searches in local storage
-      }} 
-      traileringIcon={'filter'}
-      onTraileringIconPress={() => { 
-        //open filter modal
-        alert('Filter button pressed')
-      }}
-      />
 
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Feed Page! This is an example.</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
+        <ThemedView style={styles.titleContainer}>
+          <ThemedText type="title">Feed Page! This is an example.</ThemedText>
+          <HelloWave />
+        </ThemedView>
+        <ThemedView style={styles.stepContainer}>
+          <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+          <ThemedText>
+            Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
+            Press{' '}
+            <ThemedText type="defaultSemiBold">
+              {Platform.select({
+                ios: 'cmd + d',
+                android: 'cmd + m',
+                web: 'F12',
+              })}
+            </ThemedText>{' '}
+            to open developer tools.
+          </ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.stepContainer}>
+          <Link href="/modal">
+            <Link.Trigger>
+              <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+            </Link.Trigger>
+            <Link.Preview />
+            <Link.Menu>
+              <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
               <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
+                title="Share"
+                icon="square.and.arrow.up"
+                onPress={() => alert('Share pressed')}
               />
+              <Link.Menu title="More" icon="ellipsis">
+                <Link.MenuAction
+                  title="Delete"
+                  icon="trash"
+                  destructive
+                  onPress={() => alert('Delete pressed')}
+                />
+              </Link.Menu>
             </Link.Menu>
-          </Link.Menu>
-        </Link>
+          </Link>
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+          <ThemedText>
+            {`Tap the Explore tab to learn more about what's included in this starter app.`}
+          </ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.stepContainer}>
+          <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
+          <ThemedText>
+            {`When you're ready, run `}
+            <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
+            <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
+            <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
+            <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+          </ThemedText>
+        </ThemedView>
+      </ParallaxScrollView>
+      <SearchModal
+        Visible={isModalVisible}
+        OnClose={handleCloseModal}
+        OnSearch={handleSearch}
+      />
+    </>
   );
 }
 
