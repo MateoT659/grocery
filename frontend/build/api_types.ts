83a8235674b81ba7957f90e3a5198dbeb2a11c72
saieda <saieda@udel.edu>
2025-10-22 10:55:49 -0400

    ignored build in frontend

--- frontend/build/api_types.ts
+++
@@ -1,6 +0,0 @@
-import { components } from '@/build/api_dto';
-
-export type PutExampleClassDto = components["schemas"]["PutExampleClassDto"];
-export type PutResponseExampleClassDto = components["schemas"]["PutResponseExampleClassDto"];
-export type ExampleColor = components["schemas"]["ExampleColor"];
-export type GetExampleClassDto = components["schemas"]["GetExampleClassDto"];
