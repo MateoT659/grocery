c5a51b04ef47f6da8e713ef3edbc79d5de7011d1
saieda <saieda@udel.edu>
2025-10-23 10:09:02 -0400

    trying to create a record for ingredients.

--- backend/src/main/java/com/ud11/groceries/classes/Recipe.java
+++ backend/src/main/java/com/ud11/groceries/classes/Recipe.java
@@ -4,20 +4,33 @@ import java.util.ArrayList;
 import lombok.Getter;
 import lombok.Setter;
 
+/*public record IngredientWrapper(
+        Ingredient ingredient,
+        int quantity,
+        Unit unit,
+        String notes
+) {
+    // optional: convenience constructor if you don’t always want to pass notes
+    public IngredientWrapper(Ingredient ingredient, int quantity, Unit unit) {
+        this(ingredient, quantity, unit, "");
+    }
+}
+
+
 @Getter @Setter
 public class Recipe {
     //attributes
     private String name;
-    private ArrayList<Ingredient> ingredient;
-    private int time;
+    private ArrayList<IngredientWrapper> ingredients;
+    private int timeToMakeMin;
     private String description;
 
     //constructor
-    public Recipe(String name, ArrayList<Ingredient> ingredient, int time, String description) {
+    public Recipe(String name, ArrayList<Ingredient> ingredient, int timeToMakeMin, String description) {
         this.name = name;
         this.ingredient = ingredient;
-        this.time = time;
+        this.timeToMakeMin = timeToMakeMin;
         this.description = description;
     }
 
-}
+}*/
