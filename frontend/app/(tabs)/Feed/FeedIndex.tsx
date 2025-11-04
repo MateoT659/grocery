a91103e690f99671427d9387f9cfc3c3e8d531eb
Mateo Torres <mateot659@gmail.com>
2025-11-04 15:50:32 -0500

    refactored feed pages

--- frontend/app/(tabs)/Feed/FeedIndex.tsx
+++ frontend/app/(tabs)/Feed/FeedIndex.tsx
@@ -1,147 +1,99 @@
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed/themed-text';
-import { ThemedView } from '@/components/themed/themed-view';
-import { Image } from 'expo-image';
-import { Link } from 'expo-router';
+import { ThemedSafeAreaView } from '@/components/themed/themed-safe-area-view';
+import { useRouter } from 'expo-router';
 import { useRef, useState } from 'react';
-import { Platform, StyleSheet, TextInput } from 'react-native';
+import { StyleSheet, TextInput } from 'react-native';
 import { Searchbar } from 'react-native-paper';
-import SearchModal from './SearchModal';
-import FilterModal from './FilterModal'
-import { FilterKey} from '../../../constants/FilterOptions';
+import FeedPage from './FeedPage';
+import SearchPage from './SearchPage';
 
 export default function HomeScreen() {
+  const router = useRouter();
+  //acts as a routing page to route data and the user between the feed page, search page, and filter modal
+
   // searchbar state
+  const [searchbarFocused, setSearchbarFocused] = useState(false);
+
   const [searchQuery, setSearchQuery] = useState('');
-  // search modal state
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  // filter modal state
-  const [isFilterVisible, setIsFilterVisible] = useState(false);
-  const [selectedFilters, setSelectedFilters] = useState<FilterKey[]>([]);
+  const [recentSearches, setRecentSearches] = useState<string[]>(["Eggs", "Milk", "Bread"]);
 
   const searchbarRef = useRef<TextInput>(null);
 
   const handleSearchPress = () => {
-    setIsModalVisible(true);
-    if (searchbarRef.current) {
-      searchbarRef.current.blur();
-    }
+    setSearchbarFocused(true);
   };
 
-  const handleCloseModal = () => {
-    setIsModalVisible(false);
+  const handleSearchCancel = () => {
+    setSearchbarFocused(false);
     setSearchQuery('');
   };
 
-  const handleSearch = (query: string) => {
-    console.log('Searching for:', query);
-    // search logic here
+  const handleSearch = () => {
+    const q = searchQuery.trim();
+    if (!q) return;
+
+    if (!recentSearches.includes(q)) {
+      setRecentSearches([q, ...recentSearches]);
+    }
+    setSearchQuery('');
+
+    //handle the rest of the search by calling an API or filtering data
   };
 
-  const openFilter = () => setIsFilterVisible(true);
-
-  const applyFilters = (filters: FilterKey[]) => {
-    setSelectedFilters(filters);
-    // Example: call your data fetcher with the chosen filters
-    console.log('Apply filters:', filters);
+  const removeSearch = (term: string) => {
+    setRecentSearches(recentSearches.filter(item => item !== term));
   };
+
+  const handleFilterPress = () => {
+    if (searchbarRef.current) {
+      searchbarRef.current.blur();
+    }
+    router.push('/(tabs)/Feed/FilterModal');
+  }
+
+  
 
   return (
-    <>
-      <ParallaxScrollView
-        headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-        headerImage={
-          <Image
-          />
-        }>
-        <Searchbar
-          ref={searchbarRef} placeholder="Search" value={searchQuery} onChangeText={setSearchQuery} onFocus={handleSearchPress} onSubmitEditing={() => {
-            //search in the database for things, store recent searches in local storage
-          }}
-          traileringIcon={'filter'}
-          onTraileringIconPress={() => {
-            //open filter modal
-            //alert('Filter button pressed');
-            openFilter();
-          }}
-        />
-
-        <ThemedView style={styles.titleContainer}>
-          <ThemedText type="title">Feed Page</ThemedText>
-        </ThemedView>
-        <ThemedView style={styles.stepContainer}>
-          <ThemedText type="subtitle">Recipe 1</ThemedText>
-          <ThemedText>
-            {`Tap to explore mroe about the recipe`}
-          </ThemedText>
-        </ThemedView>
-        <ThemedView style={styles.stepContainer}>
-          <Link href="/modal">
-            <Link.Trigger>
-              <ThemedText type="subtitle">Recipe 2</ThemedText>
-            </Link.Trigger>
-            <Link.Preview />
-            <Link.Menu>
-              <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-              <Link.MenuAction
-                title="Share"
-                icon="square.and.arrow.up"
-                onPress={() => alert('Share pressed')}
-              />
-              <Link.Menu title="More" icon="ellipsis">
-                <Link.MenuAction
-                  title="Delete"
-                  icon="trash"
-                  destructive
-                  onPress={() => alert('Delete pressed')}
-                />
-              </Link.Menu>
-            </Link.Menu>
-          </Link>
-
-          <ThemedText>
-            {`Tap to explore mroe about the recipe`}
-          </ThemedText>
-        </ThemedView>
-        <ThemedView style={styles.stepContainer}>
-          <ThemedText type="subtitle">Recipe 3</ThemedText>
-          <ThemedText>
-            {`Tap to explore mroe about the recipe`}
-          </ThemedText>
-        </ThemedView>
-      </ParallaxScrollView>
-
-      <SearchModal
-        Visible={isModalVisible}
-        OnClose={handleCloseModal}
-        OnSearch={handleSearch}
+    <ThemedSafeAreaView style={styles.rootContainer}>
+      <Searchbar
+        ref={searchbarRef} placeholder="Search" value={searchQuery} 
+        onChangeText={setSearchQuery} onFocus={handleSearchPress} 
+        
+        onSubmitEditing={() => {
+        handleSearch();
+        }}
+        traileringIcon={'filter'}
+        onTraileringIconPress={handleFilterPress}
+        onBlur={handleSearchCancel}
       />
-      <FilterModal
-        visible={isFilterVisible}
-        selected={selectedFilters}
-        onClose={() => setIsFilterVisible(false)}
-        onApply={applyFilters}
-      />
-    </>
+
+        {/* eventually make it so these smoothly fade to transition */}
+      {
+        searchbarFocused ? (
+          <SearchPage recentSearches={recentSearches} removeSearch={removeSearch} />
+        ) : (
+          <FeedPage/>
+        )
+      }
+      
+    </ThemedSafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 16,
+    height: '100%',
+    width: '100%',
+  },
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     gap: 8,
+    marginVertical: 16,
   },
   stepContainer: {
     gap: 8,
     marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
   },
 });
--- frontend/app/(tabs)/Feed/FilterModal.tsx
+++ frontend/app/(tabs)/Feed/FilterModal.tsx
@@ -4,37 +4,33 @@
 4) I want the user to be able click on icons of filter and they be chossen by showing up there. 
 5) I want the user to be able to click on "x" and remove them from the seclected section on the top of the page. */
 
-import React, { useMemo, useState, useEffect } from 'react';
-import { Modal, StatusBar, StyleSheet } from 'react-native';
-import { Button, Chip, Searchbar, Text, IconButton } from 'react-native-paper';
-import { FilterKey, FilterOption, FilterOptionsArray} from '../../../constants/FilterOptions';
 import { ThemedSafeAreaView } from '@/components/themed/themed-safe-area-view';
-import {ThemedView} from '@/components/themed/themed-view'; 
-import {ThemedScrollView} from '@/components/themed/themed-scroll-view'; 
-
-
-
-
+import { ThemedScrollView } from '@/components/themed/themed-scroll-view';
+import { ThemedView } from '@/components/themed/themed-view';
+import { useRouter } from 'expo-router';
+import React, { useMemo, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { Button, Chip, IconButton, Searchbar, Text } from 'react-native-paper';
+import { FilterKey, FilterOption, FilterOptionsArray } from '../../../constants/FilterOptions';
 
 //properties 
-  type props = {
-    visible: boolean; //show or hide the filter page
-    selected: FilterKey[];//current selection
-    onClose: () => void;//cose the filter page
-    onApply:(choosenFilters: FilterKey[]) => void //send the choosen filters back to home
-  }
 
-export default function FilterModal({visible, selected, onClose, onApply}:props) {
-    // searchbar state
+export default function FilterModal() {
+    const router = useRouter();  
+
     const [FilterQuery, setFilterQuery] = useState('');
-    const [localSelected, setLocalSelected] = useState <FilterKey[]> (selected);  //local select for users=> not committing the changes
-    
-    //reset local
-    useEffect(() => setLocalSelected (selected), [selected, visible]); 
+    const[selectedFilters, setSelectedFilters] = useState<FilterKey[]>([]);
 
-    const toggle = (key: FilterKey) => {setLocalSelected((prev) => prev.includes(key) ? prev.filter(k => k !==key): [...prev, key]); }; //remove the key when selected, otherwise add it
-    const remove =  (key: FilterKey)  => {setLocalSelected((prev) => prev.filter(k => k !==key));}; //remove a key when click "x"
-    const clearAll = () => setLocalSelected([]); 
+
+    const applyFilters = (filters: FilterKey[]) => {
+        setSelectedFilters(filters);
+        // Example: call your data fetcher with the chosen filters
+        console.log('Apply filters:', filters);
+      };
+
+    const toggle = (key: FilterKey) => {setSelectedFilters((prev) => prev.includes(key) ? prev.filter(k => k !==key): [...prev, key]); }; //remove the key when selected, otherwise add it
+    const remove =  (key: FilterKey)  => {setSelectedFilters((prev) => prev.filter(k => k !==key));}; //remove a key when click "x"
+    const clearAll = () => setSelectedFilters([]); 
 
     //search filtering
     const filteredOptions: FilterOption[] = useMemo(() => {
@@ -46,28 +42,24 @@ export default function FilterModal({visible, selected, onClose, onApply}:props)
     }, [FilterQuery]);
       
   return (
-      <Modal visible={visible} animationType="slide" onRequestClose={onClose}>
-        
       <ThemedSafeAreaView style={styles.container}>
-        <StatusBar barStyle="light-content" />
-
         {/* Header and close button */}
         <ThemedView style={styles.header}>
-          <IconButton icon="close" onPress={onClose} />
+          <IconButton icon="close" onPress={() => router.back()} />
         </ThemedView>
 
         {/* Filter using-bar */}
-        <ThemedView style={{ paddingHorizontal: 16, paddingBottom: 8, backgroundColor: '#fff' }}>
+        <ThemedView style={{ paddingHorizontal: 16, paddingBottom: 8 }}>
           <Searchbar
             placeholder="Filter by" value={FilterQuery} onChangeText={setFilterQuery} autoCorrect={false} autoCapitalize="none" />
         </ThemedView>
 
         {/* choose/unchoose fitler options provided */}
         <ThemedView style={styles.selectedWrap}>
-          {localSelected.length === 0 ? (
+          {selectedFilters.length === 0 ? (
             <Text style={{ opacity: 0.6, marginLeft: 16 }}> No filters selected </Text>
           ) : (
-            localSelected.map(k => {
+            selectedFilters.map(k => {
               const opt = FilterOptionsArray.find(o => o.key === k)!;
               return (
                 <Chip key={`sel-${k}`} mode="outlined" onClose={() => remove(k)} style={styles.optionChip}>
@@ -85,7 +77,7 @@ export default function FilterModal({visible, selected, onClose, onApply}:props)
             <Chip
               key={opt.key}
               mode="outlined"
-              selected={localSelected.includes(opt.key)}
+              selected={selectedFilters.includes(opt.key)}
               onPress={() => toggle(opt.key)}
               style={styles.optionChip}
             >
@@ -96,21 +88,43 @@ export default function FilterModal({visible, selected, onClose, onApply}:props)
 
         {/* Footer */}
         <ThemedView style={styles.footer}>
-          <Button onPress={() => setLocalSelected([])}>Clear all</Button>
-          <Button mode="contained" onPress={() => { onApply(localSelected); onClose(); }}>
+          <Button onPress={() => setSelectedFilters([])}>Clear all</Button>
+          <Button mode="contained" onPress={() => { /*apply to a context*/; router.back(); }}>
             Apply
           </Button>
         </ThemedView>
       </ThemedSafeAreaView>
-    </Modal>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {flex: 3, backgroundColor: '#fff' },
-  header: { paddingHorizontal: 8, paddingVertical: 4, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', backgroundColor: '#fff' },
-  selectedWrap: { flexDirection: 'row', flexWrap: 'wrap', gap: 10, paddingHorizontal: 16, paddingVertical: 16},
-  optionsWrap: { flexDirection: 'row', flexWrap: 'wrap', gap: 10, padding: 16 },
+  container: { 
+    flex: 3,
+  },
+  header: {
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  selectedWrap: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    gap: 10,
+    paddingHorizontal: 16,
+    paddingVertical: 16,
+  },
+  optionsWrap: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    gap: 10,
+    padding: 16,
+  },
   optionChip: { marginRight: 6, marginBottom: 6 },
-  footer: { flexDirection: 'row', justifyContent: 'space-between', padding: 16, borderTopWidth: StyleSheet.hairlineWidth, borderTopColor: '#eee' },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 16,
+  },
 });
