a0404f587be9a0349e40d4220f9f44f6401dd5c0
saieda <saieda@udel.edu>
2025-11-04 14:00:44 -0500

    cleaned the feed page ;)

--- frontend/app/(tabs)/Feed/FeedIndex.tsx
+++ frontend/app/(tabs)/Feed/FeedIndex.tsx
@@ -53,8 +53,6 @@ export default function HomeScreen() {
         headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
         headerImage={
           <Image
-            source={require('@/assets/images/partial-react-logo.png')}
-            style={styles.reactLogo}
           />
         }>
         <Searchbar
@@ -70,28 +68,18 @@ export default function HomeScreen() {
         />
 
         <ThemedView style={styles.titleContainer}>
-          <ThemedText type="title">Feed Page! This is an example.</ThemedText>
-          <HelloWave />
+          <ThemedText type="title">Feed Page</ThemedText>
         </ThemedView>
         <ThemedView style={styles.stepContainer}>
-          <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+          <ThemedText type="subtitle">Recipe 1</ThemedText>
           <ThemedText>
-            Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-            Press{' '}
-            <ThemedText type="defaultSemiBold">
-              {Platform.select({
-                ios: 'cmd + d',
-                android: 'cmd + m',
-                web: 'F12',
-              })}
-            </ThemedText>{' '}
-            to open developer tools.
+            {`Tap to explore mroe about the recipe`}
           </ThemedText>
         </ThemedView>
         <ThemedView style={styles.stepContainer}>
           <Link href="/modal">
             <Link.Trigger>
-              <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+              <ThemedText type="subtitle">Recipe 2</ThemedText>
             </Link.Trigger>
             <Link.Preview />
             <Link.Menu>
@@ -113,17 +101,13 @@ export default function HomeScreen() {
           </Link>
 
           <ThemedText>
-            {`Tap the Explore tab to learn more about what's included in this starter app.`}
+            {`Tap to explore mroe about the recipe`}
           </ThemedText>
         </ThemedView>
         <ThemedView style={styles.stepContainer}>
-          <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
+          <ThemedText type="subtitle">Recipe 3</ThemedText>
           <ThemedText>
-            {`When you're ready, run `}
-            <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-            <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-            <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-            <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+            {`Tap to explore mroe about the recipe`}
           </ThemedText>
         </ThemedView>
       </ParallaxScrollView>
