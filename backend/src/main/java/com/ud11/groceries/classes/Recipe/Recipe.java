36c6b9a0ad115b9d3298b9919fafe1ffc27f0ca9
Olivia <okarney@udel.edu>
2025-11-26 20:37:12 -0500

    Added tags to recipes class and recipe.json database.

--- backend/src/main/java/com/ud11/groceries/classes/Recipe/Recipe.java
+++ backend/src/main/java/com/ud11/groceries/classes/Recipe/Recipe.java
@@ -1,6 +1,8 @@
 package com.ud11.groceries.classes.Recipe;
 
 import java.util.ArrayList;
+
+import com.ud11.groceries.classes.RecipeTag;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,13 +15,15 @@ public class Recipe {
     private ArrayList<RecipeIngredientWrapper> ingredients; // list of wrappers now
     private int timeToMakeMin;
     private String description;
+    private ArrayList<RecipeTag> tags;
 
-    public Recipe(long id, String name, ArrayList<RecipeIngredientWrapper> ingredients, int timeToMakeMin, String description) {
+    public Recipe(long id, String name, ArrayList<RecipeIngredientWrapper> ingredients, int timeToMakeMin, String description, ArrayList<RecipeTag> tags) {
         this.id = id;
         this.name = name;
         this.ingredients = ingredients;
         this.timeToMakeMin = timeToMakeMin;
         this.description = description;
+        this.tags = tags;
     }
 
     @Override
@@ -30,6 +34,7 @@ public class Recipe {
                 ", ingredients=" + ingredients +
                 ", timeToMakeMin=" + timeToMakeMin +
                 ", description='" + description + '\'' +
+                ", tags=" + tags +
                 '}';
     }
 }
---
+++ backend/src/main/java/com/ud11/groceries/classes/RecipeTag.java
@@ -0,0 +1,24 @@
+package com.ud11.groceries.classes;
+
+public enum RecipeTag {
+    VEGETARIAN,
+    VEGAN,
+    HALAL,
+    KOSHER,
+    GLUTEN_FREE,
+    BREAKFAST,
+    LUNCH,
+    DINNER,
+    THAI,
+    INDIAN,
+    MEXICAN,
+    VIETNAMESE,
+    MIDDLE_EASTERN,
+    SPANISH,
+    AMERICAN,
+    ITALIAN,
+    CHINESE,
+    COLOMBIAN,
+    JAPANESE,
+    COMFORT
+}
