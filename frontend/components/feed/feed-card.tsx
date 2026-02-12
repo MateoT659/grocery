153929e57d6ae3ad4d937d8c1b3c473af2a26c56
Mateo Torres <mateot659@gmail.com>
2026-02-12 17:52:53 -0500

    found new method to use images - uri objects instead of required assets. this allows us to add custom images for each recipe by adding an image url tag in the recipe class. next step is to modify the recipe class to store image links and make them null-safe

--- frontend/components/feed/feed-card.tsx
+++ frontend/components/feed/feed-card.tsx
@@ -1,109 +1,125 @@
-import { Ionicons } from '@expo/vector-icons';
+import { Recipe } from "@/build/api_types";
+import { UserContext } from "@/contexts/user-context";
+import { updateUserFields } from "@/requests/Users";
+import { Ionicons } from "@expo/vector-icons";
 import { useContext, useState } from "react";
-import { Image, Pressable, Text, StyleSheet } from "react-native";
+import { Image, Pressable, StyleSheet } from "react-native";
 import { ThemedText } from "../themed/themed-text";
 import { ThemedView } from "../themed/themed-view";
-import { UserContext } from "@/contexts/user-context";
-import { Recipe } from "@/build/api_types";
-import { updateUserFields } from "@/requests/Users";
 
 type FeedCardProps = {
-    recipe: Recipe;
-    isFavRecipe: boolean;
-    onPress: () => void;
-    id: number;
-}
+  recipe: Recipe;
+  isFavRecipe: boolean;
+  onPress: () => void;
+  id: number;
+};
 
 export const imageSources = [
-    require('@/assets/images/arayes.png'),
-    require('@/assets/images/arroz.jpeg'),
-    require('@/assets/images/ribs.jpg'),
-    require('@/assets/images/bbqchick.jpeg'),
-
+  require("@/assets/images/arayes.png"),
+  require("@/assets/images/arroz.jpeg"),
+  require("@/assets/images/ribs.jpg"),
+  require("@/assets/images/bbqchick.jpeg"),
+  {
+    uri: "https://www.homemadeinterest.com/wp-content/uploads/2021/02/Instant-Pot-Black-Beans_IG-3.jpg",
+  },
+  {
+    uri: "https://hips.hearstapps.com/hmg-prod/images/best-homemade-pancakes-index-640775a2dbad8.jpg?crop=0.6667877686951256xw:1xh;center,top&resize=1200:*",
+  },
 ];
 
-export default function FeedCard({onPress, recipe, isFavRecipe}: FeedCardProps) {
-    const userContext = useContext(UserContext);
-    
-    const [likedRecipe, setLikedRecipe] = useState(isFavRecipe ?? false);
+export default function FeedCard({
+  onPress,
+  recipe,
+  isFavRecipe,
+}: FeedCardProps) {
+  const userContext = useContext(UserContext);
 
-    const handleLikeRecipe = async (recipeId: number) => {
-        setLikedRecipe(!likedRecipe)
-        
-        const currentLikedRecipes = userContext?.user?.likedRecipes ?? [];    
-    
-        let updatedLikedRecipes: number[];
-        
-        if (currentLikedRecipes.includes(recipeId)) {
-          updatedLikedRecipes = currentLikedRecipes.filter(r => r !== recipeId);
-        }
-        else {
-          updatedLikedRecipes = [...currentLikedRecipes, recipeId];
-        }
-    
-        const updatedLikedRecipesBackend = await updateUserFields(userContext.user?.id, { likedRecipes: updatedLikedRecipes });
-        userContext?.updateUserField('likedRecipes', updatedLikedRecipes)
-       
+  const [likedRecipe, setLikedRecipe] = useState(isFavRecipe ?? false);
+
+  const handleLikeRecipe = async (recipeId: number) => {
+    setLikedRecipe(!likedRecipe);
+
+    const currentLikedRecipes = userContext?.user?.likedRecipes ?? [];
+
+    let updatedLikedRecipes: number[];
+
+    if (currentLikedRecipes.includes(recipeId)) {
+      updatedLikedRecipes = currentLikedRecipes.filter((r) => r !== recipeId);
+    } else {
+      updatedLikedRecipes = [...currentLikedRecipes, recipeId];
     }
 
+    const updatedLikedRecipesBackend = await updateUserFields(
+      userContext.user?.id,
+      { likedRecipes: updatedLikedRecipes },
+    );
+    userContext?.updateUserField("likedRecipes", updatedLikedRecipes);
+  };
 
-    return (
-        <Pressable style={styles.feed_card} onPress={onPress}>
-            <ThemedView style={styles.card_body}>
-                <ThemedView style={styles.right_side}>
-                    <ThemedText type="subtitle" style={styles.title_text}>{recipe?.name}</ThemedText>
-                    <ThemedText style={styles.description_text}>{recipe?.description}</ThemedText>
-                </ThemedView>
-                <Image source={imageSources[recipe.id%4]} style={styles.image} />
-            </ThemedView>
-            <ThemedView style={styles.icons}>
-                <Pressable onPress={() => handleLikeRecipe(recipe?.id)}>
-                    <Ionicons 
-                        name={likedRecipe ? 'heart' : 'heart-outline'} 
-                        size={40}
-                        color={likedRecipe ? '#a11b1b' : 'black'} 
-                    />
-                </Pressable>
-            </ThemedView>
+  return (
+    <Pressable style={styles.feed_card} onPress={onPress}>
+      <ThemedView style={styles.card_body}>
+        <ThemedView style={styles.right_side}>
+          <ThemedText type="subtitle" style={styles.title_text}>
+            {recipe?.name}
+          </ThemedText>
+          <ThemedText style={styles.description_text}>
+            {recipe?.description}
+          </ThemedText>
+        </ThemedView>
+        <Image
+          source={imageSources[recipe.id % imageSources.length]}
+          style={styles.image}
+        />
+      </ThemedView>
+      <ThemedView style={styles.icons}>
+        <Pressable onPress={() => handleLikeRecipe(recipe?.id)}>
+          <Ionicons
+            name={likedRecipe ? "heart" : "heart-outline"}
+            size={40}
+            color={likedRecipe ? "#a11b1b" : "black"}
+          />
         </Pressable>
-    )
+      </ThemedView>
+    </Pressable>
+  );
 }
 
 const styles = StyleSheet.create({
-    feed_card: {
-        backgroundColor: '#f5f2f7ff',
-        borderRadius: 10,
-        padding: 15,
-        width: '100%'
-    },
-    card_body: {
-        flexDirection: 'row',
-        backgroundColor: '#f5f2f7ff',
-        justifyContent: 'space-between',
-        gap: 10,
-        alignItems: 'center'
-    },
-    right_side: {
-        backgroundColor: '#f5f2f7ff',
-        flex: 1,
-    },
-    title_text: {
-        color: 'black',
-        marginBottom: 10,
-    },
-    description_text: {
-        color: 'black',
-        flexWrap: 'wrap'
-    },
-    image: {
-        width: 150,
-        height: 150,
-        borderRadius: 10
-    },
-    icons: {
-        marginTop: 10,
-        display: 'flex',
-        alignItems: 'flex-end',
-        backgroundColor: '#f5f2f7ff'
-    }
-})
+  feed_card: {
+    backgroundColor: "#f5f2f7ff",
+    borderRadius: 10,
+    padding: 15,
+    width: "100%",
+  },
+  card_body: {
+    flexDirection: "row",
+    backgroundColor: "#f5f2f7ff",
+    justifyContent: "space-between",
+    gap: 10,
+    alignItems: "center",
+  },
+  right_side: {
+    backgroundColor: "#f5f2f7ff",
+    flex: 1,
+  },
+  title_text: {
+    color: "black",
+    marginBottom: 10,
+  },
+  description_text: {
+    color: "black",
+    flexWrap: "wrap",
+  },
+  image: {
+    width: 150,
+    height: 150,
+    borderRadius: 10,
+  },
+  icons: {
+    marginTop: 10,
+    display: "flex",
+    alignItems: "flex-end",
+    backgroundColor: "#f5f2f7ff",
+  },
+});
