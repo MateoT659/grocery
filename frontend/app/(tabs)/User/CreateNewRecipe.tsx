3ac52f5e34c7c119d45ebdcfe970cb4195e11507
Olivia <okarney@udel.edu>
2026-03-31 15:27:56 -0400

    Partial frontend for creating new recipes

---
+++ frontend/app/(tabs)/User/CreateNewRecipe.tsx
@@ -0,0 +1,252 @@
+import { Recipe } from "@/build/api_types";
+import { imageSources } from "@/components/feed/feed-card";
+import { TAG_ICONS } from "@/components/feed/tagicons";
+import SettingsButton from '@/components/settings/settings-buttons';
+import { ThemedSafeAreaView } from "@/components/themed/themed-safe-area-view";
+import { ThemedText } from "@/components/themed/themed-text";
+import { ThemedView } from "@/components/themed/themed-view";
+import { useThemeColor } from "@/hooks/use-theme-color";
+import { getRecipeById, patchRecipe } from "@/requests/Recipes";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import React from "react";
+import { Image, KeyboardAvoidingView, Platform, ScrollView, StyleSheet } from 'react-native';
+import { TextInput } from "react-native-paper";
+import FilterHeader from '@/components/chevron-back';
+import NewRecipeButton from "@/components/create-new-recipe-button";
+
+
+export default function ViewPost() {
+    const router = useRouter();
+    
+    const { id: recipe_id } = useLocalSearchParams<{ id: string }>();
+    const [recipeTitle, setRecipeTitle] = React.useState("");
+    const [recipe, setRecipe] = React.useState<Recipe | null>(null);
+    const [isEditing, setIsEditing] = React.useState(false);
+    const [description, setDescription] = React.useState("");
+    const [timeToPrep, setTimeToPrep] = React.useState("");
+    const [timeToCook, setTimeToCook] = React.useState("");
+    const [timeTotal, setTimeTotal] = React.useState("");
+    const [instructions, setInstruction] = React.useState("");
+  
+    const badgeBackground = useThemeColor({}, "card");
+    const badgeTextColor = useThemeColor({}, "text");
+    
+    React.useEffect(() => {
+      if (!recipe_id) return;
+      
+      getRecipeById(recipe_id).then((data) => {
+        setRecipe(data);
+        setDescription(data.description);
+        setTimeToPrep(String(data.timeToPrep));
+        setTimeToCook(String(data.timeToCook));
+        setTimeTotal(String(data.timeTotal));
+        setInstruction(String(data.instructions));
+        });
+    }, [recipe_id]);
+
+//   if (!recipe) {
+//     return (
+//       <ThemedSafeAreaView style={styles.safeAreaContainer}>
+//         <ThemedText>Loading recipe...</ThemedText>
+//       </ThemedSafeAreaView>
+//     );
+//   }
+
+  return (
+    <ThemedSafeAreaView style={styles.safeAreaContainer}>
+      <FilterHeader />
+        <KeyboardAvoidingView style={styles.keyboardContainer} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+          <ScrollView style={styles.scrollContainer} keyboardShouldPersistTaps='handled' contentContainerStyle={{ paddingBottom: 10 }}>
+            <ThemedView style={styles.titleContainer}>
+              <ThemedText type="title">Create New Recipe</ThemedText>
+            </ThemedView>
+            
+            <ThemedView>
+                <ThemedText type='subtitle' style={styles.subtitle}>Recipe Title</ThemedText>
+                <TextInput
+                        value={recipeTitle}
+                        onChangeText={setRecipeTitle}
+                        style={styles.inputBox}
+                    />
+            </ThemedView>
+
+            {/* <Image
+              source={
+                recipe.imageUrl
+                  ? { uri: recipe.imageUrl }
+                  : imageSources[recipe.id % imageSources.length]
+              }
+              style={styles.image}
+            /> */}
+            <ThemedView style={styles.mainPage}>
+              <ThemedView>
+                <ThemedText type='subtitle' style={styles.subtitle}>Description</ThemedText>
+                  <TextInput
+                    value={description}
+                    onChangeText={setDescription}
+                    multiline
+                    style={styles.inputBox}
+
+                  />
+              </ThemedView>
+
+              {/* <ThemedView style={styles.tagContainer}>
+                {recipe.tags
+                  ?.filter((tag) => TAG_ICONS[tag])
+                  .map((tag) => (
+                    <ThemedView
+                      key={tag}
+                      style={[
+                        styles.tagBadge,
+                        { backgroundColor: badgeBackground },
+                      ]}
+                    >
+                      <ThemedText style={{ color: badgeTextColor }}>
+                        {TAG_ICONS[tag]} {tag.replaceAll("_", " ")}
+                      </ThemedText>
+                    </ThemedView>
+                  ))}
+              </ThemedView> */}
+
+              <ThemedView style={styles.timeInfo}>
+                <ThemedView style={styles.timeInfoSection}>
+                  <ThemedText type="defaultSemiBold">Prep Time</ThemedText>
+                    <TextInput
+                      value={timeToPrep}
+                      onChangeText={setTimeToPrep}
+                      style={[styles.timeRequired, styles.timeTrack]}
+                    />
+                  <ThemedText>minutes</ThemedText>
+                </ThemedView>
+
+                <ThemedView style={styles.timeInfoSection}>
+                  <ThemedText type="defaultSemiBold">Cook Time</ThemedText>
+                    <TextInput
+                      value={timeToCook}
+                      onChangeText={setTimeToCook}
+                      style={[styles.timeRequired, styles.timeTrack]}
+                    />
+                  <ThemedText>minutes</ThemedText>
+                </ThemedView>
+                <ThemedView style={styles.timeInfoSection}>
+                  <ThemedText type="defaultSemiBold">Total Time</ThemedText>
+                    <TextInput
+                      value={timeTotal}
+                      onChangeText={setTimeTotal}
+                      style={[styles.timeRequired, styles.timeTrack]}
+                    />
+                  <ThemedText>minutes</ThemedText>
+                </ThemedView>
+              </ThemedView>
+
+              <ThemedView>
+                <ThemedText type="subtitle" style={styles.subtitle}>
+                  Ingredients
+                </ThemedText>
+                {/* <ThemedView style={styles.stepContainer}>
+                  {recipe.ingredients.map((riw, index) => (
+                    <ThemedText key={index} style={{ fontSize: 14 }}>
+                      - {riw.ingredientDisplayName}
+                    </ThemedText>
+                  ))}
+                </ThemedView> */}
+              </ThemedView>
+
+              <ThemedView>
+                <ThemedText type="subtitle" style={styles.subtitle}>Instructions</ThemedText>
+                  <TextInput
+                    value={instructions}
+                    onChangeText={setInstruction}
+                    multiline
+                  />
+                </ThemedView>
+
+              <NewRecipeButton
+                title="Create Recipe" 
+                onPress={() => console.log("done!")}
+                // onPress={() => router.push(`/(tabs)/Feed/ViewPost/${recipe.id.toString()}`)}
+              />
+
+            </ThemedView>
+          </ScrollView>
+        </KeyboardAvoidingView>
+    </ThemedSafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeAreaContainer: {
+    flex: 1,
+  },
+  scrollContainer: {
+    flex: 1,
+    margin: 15,
+  },
+  keyboardContainer: {
+    flex: 1,
+  },
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+    marginTop: 20,
+    marginBottom: 15,
+  },
+  mainPage: {
+    gap: 20,
+  },
+  timeInfo: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  timeInfoSection: {
+    display: "flex",
+    alignItems: "center",
+  },
+  timeRequired: {
+    fontSize: 20,
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginBottom: 8,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    borderRadius: 10,
+    marginBottom: 15
+  },
+  inputBox: {
+    marginBottom: 10,
+  },
+  editButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 100,
+    marginLeft: 70,
+  },
+  timeTrack: {
+    width: 60, 
+    height: 30, 
+    borderWidth: 1, 
+    textAlign: 'center'
+  },
+  tagContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    marginVertical: 10,
+  },
+  tagBadge: {
+    borderRadius: 14,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginRight: 6,
+    marginBottom: 6,
+  },
+  tagText: {
+    fontSize: 12,
+  },
+});
