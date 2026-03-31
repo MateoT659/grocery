3001caf9a251cbe0ed4b345784e6329a94c6b8ea
saieda <saieda@udel.edu>
2026-03-31 19:07:01 -0400

    fixed the error: property id is missing in type on the Favorite page.

--- frontend/app/(tabs)/User/FavoritesPage.tsx
+++ frontend/app/(tabs)/User/FavoritesPage.tsx
@@ -45,6 +45,7 @@ export default function FavoritesPage() {
             (favRecipes.map((recipe) => (
                 <FeedCard 
                     key={recipe.id}
+                    id={recipe.id}
                     onPress={() => router.push(`/(tabs)/Feed/ViewPost/${recipe.id.toString()}`)}
                     recipe={recipe} isFavRecipe={favRecipeIds?.includes(recipe.id) ? true : false}>
 
