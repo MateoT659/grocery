7f827cda401a2cae15954670c0c31372203f5278
Mateo Torres <mateot659@gmail.com>
2026-04-10 14:28:40 -0400

    added choosing recipes for a grocery list generation (not pretty, just functional)

--- frontend/app/(tabs)/Lists/CreateModal.tsx
+++ frontend/app/(tabs)/Lists/CreateModal.tsx
@@ -1,35 +1,93 @@
-import { GroceryList, Ingredient } from '@/build/api_types'
-import CreateModalHeader from '@/components/lists/create-modal-header'
-import TabSeparator from '@/components/settings/tab-seperator'
-import { ThemedScrollView } from '@/components/themed/themed-scroll-view'
-import { ThemedText } from '@/components/themed/themed-text'
-import { ThemedTextInput } from '@/components/themed/themed-text-input'
-import { ThemedView } from '@/components/themed/themed-view'
-import { addGroceryList, generateGroceryList } from '@/requests/GroceryLists'
-import getAllIngredients from '@/requests/Ingredients'
-import { wrapIngredientForList } from '@/utils/Ingredient'
-import { Ionicons } from '@expo/vector-icons'
-import { useRouter } from 'expo-router'
-import React, { useEffect } from 'react'
-import { StyleSheet, TouchableOpacity } from 'react-native'
+import { GroceryList, Ingredient, Recipe } from "@/build/api_types";
+import CreateModalHeader from "@/components/lists/create-modal-header";
+import TabSeparator from "@/components/settings/tab-seperator";
+import { ThemedScrollView } from "@/components/themed/themed-scroll-view";
+import { ThemedText } from "@/components/themed/themed-text";
+import { ThemedTextInput } from "@/components/themed/themed-text-input";
+import { ThemedView } from "@/components/themed/themed-view";
+import { addGroceryList, generateGroceryList } from "@/requests/GroceryLists";
+import getAllIngredients from "@/requests/Ingredients";
+import getAllRecipes from "@/requests/Recipes";
+import { wrapIngredientForList } from "@/utils/Ingredient";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import React, { useEffect } from "react";
+import {
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 
 export default function CreateModal() {
-  const DEFAULT_GROCERY_LIST: GroceryList = {id: -1, name: '', description: '', items: [], recipes: []};
-  const [groceryList, setGroceryList] = React.useState<GroceryList>(DEFAULT_GROCERY_LIST);
+  const DEFAULT_GROCERY_LIST: GroceryList = {
+    id: -1,
+    name: "",
+    description: "",
+    items: [],
+    recipes: [],
+  };
+  const [groceryList, setGroceryList] =
+    React.useState<GroceryList>(DEFAULT_GROCERY_LIST);
   const [nRecipes, setNRecipes] = React.useState<number>(0);
   const [page, setPage] = React.useState<number>(0);
   const router = useRouter();
 
   const [ingredients, setIngredients] = React.useState<Ingredient[]>([]);
-  const [selectedIngredients, setSelectedIngredients] = React.useState<Ingredient[]>([]);
+  const [selectedIngredients, setSelectedIngredients] = React.useState<
+    Ingredient[]
+  >([]);
 
-  const [missedRequiredFields, setMissedRequiredFields] = React.useState<boolean>(false);
+  const [missedRequiredFields, setMissedRequiredFields] =
+    React.useState<boolean>(false);
 
   const [manualEnter, setManualEnter] = React.useState<boolean>(false);
+
+  const [recipeSeed, setRecipeSeed] = React.useState<number[]>([]);
+
+  const [allRecipes, setAllRecipes] = React.useState<Recipe[]>([]);
+
+  function RecipeCard({ recipe }: { recipe: Recipe }) {
+    return (
+      <Pressable
+        onPress={() => {
+          if (recipeSeed.includes(recipe.id)) {
+            setRecipeSeed(recipeSeed.filter((id) => id !== recipe.id));
+          } else {
+            setRecipeSeed([...recipeSeed, recipe.id]);
+          }
+        }}
+      >
+        <ThemedView
+          style={{
+            padding: 10,
+            borderWidth: 1,
+            borderColor: "gray",
+            marginRight: 10,
+            borderRadius: 8,
+            backgroundColor: recipeSeed.includes(recipe.id)
+              ? "#81b0ff"
+              : "transparent",
+          }}
+        >
+          <ThemedText
+            style={{
+              color: recipeSeed.includes(recipe.id) ? "black" : "white",
+            }}
+          >
+            {recipe.name}
+          </ThemedText>
+        </ThemedView>
+      </Pressable>
+    );
+  }
 
   useEffect(() => {
     getAllIngredients().then((fetchedIngredients) => {
       setIngredients(fetchedIngredients);
+    });
+    getAllRecipes().then((fetchedRecipes) => {
+      setAllRecipes(fetchedRecipes);
     });
   }, []);
 
@@ -42,16 +100,18 @@ export default function CreateModal() {
   };
 
   const isIngredientSelected = (ingredient: Ingredient) => {
-    return selectedIngredients.some(item => item.id === ingredient.id);
-  }
+    return selectedIngredients.some((item) => item.id === ingredient.id);
+  };
 
   const handleAddIngredient = (ingredient: Ingredient) => {
-    const existingItem = groceryList.items.find(item => item.ingredientId === ingredient.id);
+    const existingItem = groceryList.items.find(
+      (item) => item.ingredientId === ingredient.id,
+    );
     if (!existingItem) {
       const newItem = { ingredientId: ingredient.id, checked: false };
       setGroceryList({
         ...groceryList,
-        items: [...groceryList.items, wrapIngredientForList(ingredient)]
+        items: [...groceryList.items, wrapIngredientForList(ingredient)],
       });
       setSelectedIngredients([...selectedIngredients, ingredient]);
     }
@@ -60,11 +120,14 @@ export default function CreateModal() {
   const handleRemoveIngredient = (ingredient: Ingredient) => {
     setGroceryList({
       ...groceryList,
-      items: groceryList.items.filter(item => item.ingredientId !== ingredient.id)
+      items: groceryList.items.filter(
+        (item) => item.ingredientId !== ingredient.id,
+      ),
     });
-    setSelectedIngredients(selectedIngredients.filter(item => item.id !== ingredient.id));
-  }
-  
+    setSelectedIngredients(
+      selectedIngredients.filter((item) => item.id !== ingredient.id),
+    );
+  };
 
   const nextPage = () => {
     setMissedRequiredFields(false);
@@ -78,41 +141,44 @@ export default function CreateModal() {
       return;
     }
 
-    if(page == 1 && !manualEnter && (nRecipes <= 0 || isNaN(nRecipes))) {
+    if (page == 1 && !manualEnter && (nRecipes <= 0 || isNaN(nRecipes))) {
       setMissedRequiredFields(true);
       return;
     }
 
     if (page == 1) {
-      if(manualEnter) {
-      //dismiss modal then view the new list
+      if (manualEnter) {
+        //dismiss modal then view the new list
         addGroceryList(groceryList).then((response) => {
-          if(!response.success) {
+          if (!response.success) {
             console.log("Failed to create grocery list:", response.message);
-            router.back();
-            return;
-          }
-          
-          router.back();
-          router.push(`/Lists/ViewList?id=${response.newGroceryList.id}`);
-        });
-      }
-      else {
-        generateGroceryList(nRecipes, groceryList).then((response) => {
-          if(!response.success) {
-            console.log("Failed to generate grocery list:", response.message);
             router.back();
             return;
           }
 
           router.back();
-          router.push(`/Lists/ViewList?id=${response.generatedGroceryList.id}`);          
+          router.push(`/Lists/ViewList?id=${response.newGroceryList.id}`);
         });
+      } else {
+        generateGroceryList(nRecipes, groceryList, recipeSeed).then(
+          (response) => {
+            if (!response.success) {
+              console.log("Failed to generate grocery list:", response.message);
+              router.back();
+              return;
+            }
+
+            router.back();
+            router.push(
+              `/Lists/ViewList?id=${response.generatedGroceryList.id}`,
+            );
+          },
+        );
       }
     }
     setMissedRequiredFields(false);
     setPage(page + 1);
-  }
+  };
 
   const lastPage = () => {
     setMissedRequiredFields(false);
@@ -121,116 +187,154 @@ export default function CreateModal() {
       router.back();
     }
     setPage(page - 1);
-  }
+  };
 
   const manualEntryPage = (
     <ThemedView style={{ paddingBottom: 32 }}>
-      { missedRequiredFields ? <ThemedText style={{ color: '#914a4aff', padding: 10 }}>Please select at least one ingredient.</ThemedText> : null }
-      {
-        ingredients.map((ingredient) => (
-          <ThemedView key={ingredient.id} style={{ flexDirection: 'row', alignItems: 'center', padding: 2 }}>
-            <ThemedText onPress={() => handleTapIngredient(ingredient)} style={{ flex: 1, fontWeight: isIngredientSelected(ingredient) ? 'bold' : 'normal' }}>{ingredient.name}{isIngredientSelected(ingredient) ? ' - ' : ' + '}</ThemedText>
-          </ThemedView>
-        ))
-      }
-      <TouchableOpacity onPress={()=> setManualEnter(!manualEnter)} style={styles.manualInputSwapContainer}>
-            <ThemedView style={styles.manualInputSwapButton}>
-              <Ionicons name='add-outline' size={20} color='cyan' />
-              <ThemedText style={styles.manualInputSwapButtonText}>Generate a List</ThemedText>
-            </ThemedView>
-         </TouchableOpacity>
+      {missedRequiredFields ? (
+        <ThemedText style={{ color: "#914a4aff", padding: 10 }}>
+          Please select at least one ingredient.
+        </ThemedText>
+      ) : null}
+      {ingredients.map((ingredient) => (
+        <ThemedView
+          key={ingredient.id}
+          style={{ flexDirection: "row", alignItems: "center", padding: 2 }}
+        >
+          <ThemedText
+            onPress={() => handleTapIngredient(ingredient)}
+            style={{
+              flex: 1,
+              fontWeight: isIngredientSelected(ingredient) ? "bold" : "normal",
+            }}
+          >
+            {ingredient.name}
+            {isIngredientSelected(ingredient) ? " - " : " + "}
+          </ThemedText>
+        </ThemedView>
+      ))}
+      <TouchableOpacity
+        onPress={() => setManualEnter(!manualEnter)}
+        style={styles.manualInputSwapContainer}
+      >
+        <ThemedView style={styles.manualInputSwapButton}>
+          <Ionicons name="add-outline" size={20} color="cyan" />
+          <ThemedText style={styles.manualInputSwapButtonText}>
+            Generate a List
+          </ThemedText>
+        </ThemedView>
+      </TouchableOpacity>
     </ThemedView>
-  )
+  );
 
   const pages = [
-    (
-      // title, description color
-      <>
-        <ThemedTextInput
-          placeholder="Name*"
-          placeholderTextColor={missedRequiredFields ? '#914a4aff' : '#545454ff'}
-          value={groceryList.name}
-          onChangeText={(text) => setGroceryList({...groceryList, name: text})}
-          style={styles.textInputs}
-        />
-        <TabSeparator color='gray' />
-        <ThemedTextInput
-          placeholder="Description*"
-          placeholderTextColor={missedRequiredFields ? '#914a4aff' : '#545454ff'}
-          value={groceryList.description}
-          onChangeText={(text) => setGroceryList({...groceryList, description: text})}
-          style={styles.textInputs}
-        />
-        <TabSeparator color='gray' />
-      </>
-    ),
-    (
-      // generation params or manual entry
-      <>
-        {manualEnter ? manualEntryPage : (
-          <>
-            <ThemedTextInput
-              keyboardType='number-pad'
-              placeholder="Number of Recipes*"
-              placeholderTextColor={missedRequiredFields ? '#914a4aff' : '#545454ff'}
-              value={nRecipes == 0 ? "" : nRecipes.toString()}
-              onChangeText={(text) => setNRecipes(isNaN(Number(text)) ? 0 : Number(text))}
-              style={styles.textInputs}
-            />
-        <TabSeparator color='gray' />
-          <TouchableOpacity onPress={()=> setManualEnter(!manualEnter)} style={styles.manualInputSwapContainer}>
-            <ThemedView style={styles.manualInputSwapButton}>
-              <Ionicons name='add-outline' size={20} color='cyan' />
-              <ThemedText style={styles.manualInputSwapButtonText}>Choose Ingredients Manually</ThemedText>
-            </ThemedView>
-         </TouchableOpacity>
-          </>
-        )}
-      </>
-    )
-  ]
+    // title, description color
+    <>
+      <ThemedTextInput
+        placeholder="Name*"
+        placeholderTextColor={missedRequiredFields ? "#914a4aff" : "#545454ff"}
+        value={groceryList.name}
+        onChangeText={(text) => setGroceryList({ ...groceryList, name: text })}
+        style={styles.textInputs}
+      />
+      <TabSeparator color="gray" />
+      <ThemedTextInput
+        placeholder="Description*"
+        placeholderTextColor={missedRequiredFields ? "#914a4aff" : "#545454ff"}
+        value={groceryList.description}
+        onChangeText={(text) =>
+          setGroceryList({ ...groceryList, description: text })
+        }
+        style={styles.textInputs}
+      />
+      <TabSeparator color="gray" />
+    </>,
+    // generation params or manual entry
+    <>
+      {manualEnter ? (
+        manualEntryPage
+      ) : (
+        <>
+          <ThemedTextInput
+            keyboardType="number-pad"
+            placeholder="Number of Recipes*"
+            placeholderTextColor={
+              missedRequiredFields ? "#914a4aff" : "#545454ff"
+            }
+            value={nRecipes == 0 ? "" : nRecipes.toString()}
+            onChangeText={(text) =>
+              setNRecipes(isNaN(Number(text)) ? 0 : Number(text))
+            }
+            style={styles.textInputs}
+          />
+          <TabSeparator color="gray" />
+          <ScrollView style={{ maxHeight: 200 }} horizontal={true}>
+            {allRecipes.map((recipe) => (
+              <RecipeCard key={recipe.id} recipe={recipe} />
+            ))}
+          </ScrollView>
+          <TabSeparator color="gray" />
 
-  
+          <TouchableOpacity
+            onPress={() => setManualEnter(!manualEnter)}
+            style={styles.manualInputSwapContainer}
+          >
+            <ThemedView style={styles.manualInputSwapButton}>
+              <Ionicons name="add-outline" size={20} color="cyan" />
+              <ThemedText style={styles.manualInputSwapButtonText}>
+                Choose Ingredients Manually
+              </ThemedText>
+            </ThemedView>
+          </TouchableOpacity>
+        </>
+      )}
+    </>,
+  ];
 
   return (
     <ThemedView style={styles.rootContainer}>
-      <CreateModalHeader leftText={["Cancel", "Back"]} rightText={["Next", "Generate"]} onLeftPress={lastPage} onRightPress={nextPage} page={page} />
+      <CreateModalHeader
+        leftText={["Cancel", "Back"]}
+        rightText={["Next", "Generate"]}
+        onLeftPress={lastPage}
+        onRightPress={nextPage}
+        page={page}
+      />
       <ThemedScrollView style={styles.internalScrollContainer}>
         {pages[page]}
       </ThemedScrollView>
     </ThemedView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   rootContainer: {
-    width: '100%',
-    height: '100%'
+    width: "100%",
+    height: "100%",
   },
   internalScrollContainer: {},
   textInputs: {
     fontSize: 18,
-    padding: 10
+    padding: 10,
   },
   manualInputSwapContainer: {
-    alignItems: 'center',
+    alignItems: "center",
     marginVertical: 16,
   },
-  manualInputSwapButton:{
-    flexDirection: 'row', 
-    alignItems: 'center', 
-    gap: 8, 
-    borderColor: 'cyan', 
-    borderWidth: 1, 
-    borderRadius: 99, 
-    padding:12,
-
+  manualInputSwapButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+    borderColor: "cyan",
+    borderWidth: 1,
+    borderRadius: 99,
+    padding: 12,
   },
   manualInputSwapButtonText: {
     fontSize: 16,
-    textAlign: 'center',
-    fontWeight: 'bold',
-    color: 'cyan',
-    borderWidth:1,
-  }
-})
+    textAlign: "center",
+    fontWeight: "bold",
+    color: "cyan",
+    borderWidth: 1,
+  },
+});
--- frontend/requests/GroceryLists.ts
+++ frontend/requests/GroceryLists.ts
@@ -1,5 +1,12 @@
-import { DEV_API_HOSTURL } from '@/.apiconfig.json';
-import { DeleteGroceryListResponseDto, GenerateGroceryListDto, GenerateGroceryListResponseDto, GroceryList, PostGroceryListResponseDto, PutGroceryListResponseDto } from "@/build/api_types";
+import { DEV_API_HOSTURL } from "@/.apiconfig.json";
+import {
+  DeleteGroceryListResponseDto,
+  GenerateGroceryListDto,
+  GenerateGroceryListResponseDto,
+  GroceryList,
+  PostGroceryListResponseDto,
+  PutGroceryListResponseDto,
+} from "@/build/api_types";
 
 const GROCERY_LIST_API_URL = `${DEV_API_HOSTURL}/grocery-list-api`;
 
@@ -8,52 +15,76 @@ export default async function getAllGroceryLists(): Promise<GroceryList[]> {
   return await response.json();
 }
 
-export async function getGroceryListById(groceryListId: string): Promise<GroceryList> {
-  const response = await fetch(`${GROCERY_LIST_API_URL}/get-grocery-list/${groceryListId.toString()}`);
+export async function getGroceryListById(
+  groceryListId: string,
+): Promise<GroceryList> {
+  const response = await fetch(
+    `${GROCERY_LIST_API_URL}/get-grocery-list/${groceryListId.toString()}`,
+  );
   return await response.json();
 }
 
-export async function setGroceryListById(groceryListId: string, updatedList: GroceryList): Promise<PutGroceryListResponseDto> {
-  const response = await fetch(`${GROCERY_LIST_API_URL}/put-grocery-list/${groceryListId.toString()}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
+export async function setGroceryListById(
+  groceryListId: string,
+  updatedList: GroceryList,
+): Promise<PutGroceryListResponseDto> {
+  const response = await fetch(
+    `${GROCERY_LIST_API_URL}/put-grocery-list/${groceryListId.toString()}`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedList),
     },
-    body: JSON.stringify(updatedList),
-  });
+  );
   return await response.json();
 }
 
-export async function addGroceryList(newList: GroceryList): Promise<PostGroceryListResponseDto> {
+export async function addGroceryList(
+  newList: GroceryList,
+): Promise<PostGroceryListResponseDto> {
   const response = await fetch(`${GROCERY_LIST_API_URL}/add-grocery-list`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(newList),
   });
   return await response.json();
 }
 
-export async function deleteGroceryListById(groceryListId: string): Promise<DeleteGroceryListResponseDto> {
-  const response = await fetch(`${GROCERY_LIST_API_URL}/delete-grocery-list/${groceryListId.toString()}`, {
-    method: 'DELETE',
-  });
+export async function deleteGroceryListById(
+  groceryListId: string,
+): Promise<DeleteGroceryListResponseDto> {
+  const response = await fetch(
+    `${GROCERY_LIST_API_URL}/delete-grocery-list/${groceryListId.toString()}`,
+    {
+      method: "DELETE",
+    },
+  );
   return await response.json();
 }
 
-export async function generateGroceryList(nRecipes: number, newList: GroceryList): Promise<GenerateGroceryListResponseDto>{
+export async function generateGroceryList(
+  nRecipes: number,
+  newList: GroceryList,
+  recipeSeed: number[],
+): Promise<GenerateGroceryListResponseDto> {
   const body: GenerateGroceryListDto = {
     nRecipes: nRecipes,
-    groceryListArgs: newList
+    groceryListArgs: newList,
+    recipeSeed: recipeSeed,
   };
-  const response = await fetch(`${GROCERY_LIST_API_URL}/generate-grocery-list`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
+  const response = await fetch(
+    `${GROCERY_LIST_API_URL}/generate-grocery-list`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
     },
-    body: JSON.stringify(body),
-  });
+  );
   return await response.json();
-
 }
